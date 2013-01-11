6b38d54a49d5e5eb4658160b3365611caab863f4
André de Oliveira <andre@arbo.com.br>
2013-01-11 20:00:24 -0200

    Trying to visit a Region with a StreamTokenizer

--- steamside-core/src/main/java/br/com/arbo/steamside/vdf/Region.java
+++ steamside-core/src/main/java/br/com/arbo/steamside/vdf/Region.java
@@ -1,12 +1,17 @@
 package br.com.arbo.steamside.vdf;
 
+import java.io.StreamTokenizer;
+import java.io.StringReader;
+
 class Region {
 
 	private final Vdf vdf;
 	private final Token head;
+	private final int from;
 
 	Region(Vdf vdf, int from) {
 		this.vdf = vdf;
+		this.from = from;
 		this.head = vdf.seek(from);
 	}
 
@@ -23,9 +28,12 @@ class Region {
 
 	interface KeyValueVisitor {
 
-		void visit(KeyValue kv);
+		void onKeyValue(String k, String v);
+
+		void onSubRegion(String k, Region r);
 	}
 
+	/*
 	void accept(KeyValueVisitor visitor) {
 		final int body = head.to + 1;
 		int pos = body;
@@ -33,6 +41,45 @@ class Region {
 			Token key = vdf.seek(pos);
 			Token value = vdf.seek(key.to + 1);
 			visitor.visit(new KeyValue(key, value));
+		}
+	}
+	*/
+
+	void accept(KeyValueVisitor visitor) {
+		StreamTokenizer parser = StreamTokenizerBuilder.build(new StringReader(
+				vdf.content.substring(this.from)));
+
+		// Read node name
+		parser.nextToken();
+		String name = parser.sval;
+
+		parser.nextToken();
+		String value = parser.sval;
+
+		// Check if next token is value or open of branch
+		if (value != null)
+		{
+			// If token is value, read value
+			visitor.onKeyValue(name, value);
+
+		} else if (parser.ttype == '{')
+		{
+			// If token is open branch, read child nodes
+
+			Region sub = new Region(vdf, parser.lineno());
+			visitor.onSubRegion(name, sub);
+
+			while (parser.nextToken() != '}')
+			{
+				parser.pushBack(); // token is meaningful, becomes available again for parsing
+
+				if (!branchNode.addNode(Node.parse(parser)))
+				{
+					throw new InvalidFileException(
+							"Could not read node from file.");
+				}
+			}
+			node = branchNode;
 		}
 	}
 }
