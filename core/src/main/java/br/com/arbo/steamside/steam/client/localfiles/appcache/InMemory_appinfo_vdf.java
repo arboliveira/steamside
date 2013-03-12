c3e1ccff8f6342ae2c1771492f8a25524ac828f8
André de Oliveira <andre@arbo.com.br>
2013-03-12 01:38:54 -0300

    Relevant links on front page + removed dead code

--- core/src/main/java/br/com/arbo/steamside/steam/client/localfiles/appcache/InMemory_appinfo_vdf.java
+++ core/src/main/java/br/com/arbo/steamside/steam/client/localfiles/appcache/InMemory_appinfo_vdf.java
@@ -6,7 +6,7 @@ import org.apache.commons.io.FileUtils;
 
 public class InMemory_appinfo_vdf implements Content_appinfo_vdf {
 
-	final String content = read_appinfo_vdf();
+	private final String content = read_appinfo_vdf();
 
 	@Override
 	public String content() {
--- webui/src/main/java/br/com/arbo/steamside/webui/wicket/HomePage.java
+++ webui/src/main/java/br/com/arbo/steamside/webui/wicket/HomePage.java
@@ -2,7 +2,6 @@ package br.com.arbo.steamside.webui.wicket;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.link.Link;
 
 import br.com.arbo.steamside.steam.client.localfiles.sharedconfig.Factory_sharedconfig_vdf.FileNotFound_sharedconfig_vdf;
 
@@ -16,33 +15,15 @@ public class HomePage extends WebPage {
 
 		final SharedConfigConsume config = new SharedConfigConsume();
 		try {
-			if (false) populateContinue();
 			populateNumbers(config);
 		} catch (final FileNotFound_sharedconfig_vdf e) {
 			// TODO Redirect to "Where is your Steam?" configuration page
 			throw e;
 		}
-
-		// TODO Add your page's components here
-
 	}
 
 	private void populateNumbers(final SharedConfigConsume config) {
 		final GamesOwned o = new GamesOwned(config);
 		add(new Label("number-of-games", String.valueOf(o.gamesOwned())));
-	}
-
-	private void populateContinue() {
-		final HomePage item = this;
-
-		final String appid = "72850";
-		final String caption = "Skyrim";
-		final Link< ? > externalLink = newLink("link", appid);
-		externalLink.add(new Label("caption", caption));
-		item.add(externalLink);
-	}
-
-	private static Link< ? > newLink(final String id, final String appid) {
-		return new ExecLink(id, appid);
 	}
 }
