fd7e24fce0813c5f47a5b9baf94ff46506db604d
André de Oliveira <andre@arbo.com.br>
2013-03-15 02:29:37 -0300

    Parse appinfo.vdf

--- core/src/test/java/br/com/arbo/steamside/steam/client/localfiles/appcache/ExampleContent.java
+++ core/src/test/java/br/com/arbo/steamside/steam/client/localfiles/appcache/ExampleContent.java
@@ -1,65 +1,123 @@
 package br.com.arbo.steamside.steam.client.localfiles.appcache;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteOrder;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileChannel.MapMode;
+
 import org.apache.commons.lang3.StringUtils;
 
 public class ExampleContent {
 
-	ExampleContent() {
-		vdf = Content_appinfo_vdf.content();
-	}
-
-	int linenumber = 0;
-	int pairmark1 = 0;
-	int pairmark2;
-
-	public static void main(final String[] args) {
+	public static void main(final String[] args) throws IOException {
 		new ExampleContent().go();
 	}
 
-	private void go() {
+	private void go() throws IOException {
+		final FileInputStream f =
+				new FileInputStream(File_appinfo_vdf.appinfo_vdf());
+		try {
+			go2(f);
+		} finally {
+			f.close();
+		}
+	}
 
-		izone += 4; // 0x07564426
-		izone += 4; // enum EUniverse
+	private void go2(final FileInputStream f) throws IOException {
+		final FileChannel ch = f.getChannel();
+		buffer = ch.map(MapMode.READ_ONLY, 0L, ch.size());
+		buffer.order(ByteOrder.LITTLE_ENDIAN);
+		go3();
+	}
 
+	private void go3() throws UnsupportedEncodingException {
+		read__uint32_t(); // 0x07564426
+		read__uint32_t(); // enum EUniverse
 		while (true) {
-			final String app_id = vdf.substring(izone, izone + 4);
+			final int app_id = read__uint32_t();
 
-			if (linenumber == 1000) break;
+			if (app_id == 0) break;
 
-			pairmark2 = vdf.indexOf(PAIR_SEPARATOR, pairmark1);
-			if (pairmark2 == -1) break;
+			final int data_size = read__uint32_t();
 
-			final String line = vdf.substring(pairmark1, pairmark2);
-			pairmark1 = pairmark2 + PAIR_SEPARATOR.length();
-
-			final int pairsplit = line.indexOf(BYTE_0);
-			final String k = line.substring(0, pairsplit);
-			final String v = line.substring(pairsplit + 1);
-
-			if (v.startsWith("Base Goldsrc Shared Platform"))
-				System.out.println(v);
-
-			linenumber++;
 			final String padded =
 					StringUtils.leftPad(
-							String.valueOf(linenumber),
-							10);
-			System.out.println(padded + ":" + k + "=" + v);
-		}
+							String.valueOf(app_id),
+							6);
+			System.out.println(padded + ":" + data_size);
 
+			final int p1 = buffer.position();
+
+			read__uint32_t(); // unknown1
+			read__uint32_t(); // last_updated
+			read__uint64_t(); // unknown2;
+			for (int i = 1; i <= 20; i++)
+				read__uint8_t(); // unknown3[20];
+			read__uint32_t(); // change_number;
+
+			final int p2 = buffer.position();
+			final int pdif = p2 - p1;
+			final int remaining = data_size - pdif;
+
+			while (true) {
+				final byte section_number = read__uint8_t(); // enum EAppInfoSection
+				if (section_number == 0) break;
+				buffer.get(); // 0x00
+				buffer.getChar(); // 0x35 0x00
+				while (true) {
+					final byte type = buffer.get();
+					if (type == 0) break;
+					final String k = read__null_terminated_string();
+					final String v = read__null_terminated_string();
+					System.out.println(k + "=" + v);
+				}
+
+				if (false) {
+					final byte[] section_data = new byte[remaining];
+					buffer.get(section_data);
+					final String s = new String(section_data, "Cp1252");
+					System.out.println(s);
+				}
+			}
+		}
+	}
+
+	private String read__null_terminated_string()
+			throws UnsupportedEncodingException {
+		int i = 0;
+		while (true) {
+			stringbuffer[i] = buffer.get();
+			if (stringbuffer[i] == 0) break;
+			i++;
+		}
+		final String sname = new String(stringbuffer, 0, i, "Cp1252");
+		return sname;
+	}
+
+	private byte read__uint8_t() {
+		return buffer.get();
+	}
+
+	private long read__uint64_t() {
+		return buffer.getLong();
+	}
+
+	private void skip(final int i) {
+		buffer.position(buffer.position() + i);
+	}
+
+	private int read__uint32_t() {
+		return buffer.getInt();
 	}
 
 	static private final char BYTE_0 = 0;
-	static private final char BYTE_1 = 1;
 	static private final char BYTE_8 = 8;
 	static private final String ZONE_SEPARATOR =
 			new String(new char[] { BYTE_0, BYTE_8, BYTE_8 });
-	static private final String PAIR_SEPARATOR =
-			new String(new char[] { BYTE_0, BYTE_1 });
 
-	static private final String magicnumber =
-			new String(new char[] { 0x26, 0x44, 0x56, 0x07 });
-	private final String vdf;
-
-	int izone;
+	private MappedByteBuffer buffer;
+	private final byte[] stringbuffer = new byte[1000];
 }
