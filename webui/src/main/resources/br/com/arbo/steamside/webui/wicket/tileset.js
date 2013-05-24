59db877f39479836e1824c5bbbfc851f6143cc4c
André de Oliveira <andre@arbo.com.br>
2013-05-23 22:32:35 -0300

    Object-oriented Tileset

--- webui/src/main/resources/br/com/arbo/steamside/webui/wicket/tileset.js
+++ webui/src/main/resources/br/com/arbo/steamside/webui/wicket/tileset.js
@@ -1,90 +1,106 @@
-var Tileset = {
-    _gameCard: null,
-    _moreButton: null,
-    _collectionPick: null,
-    _tileSteamClient: new Object,
-    _tileCollectionNew: new Object,
-	_tileCollectionEdit: new Object,
-	_tileCommandBox: new Object,
+var Tile = Backbone.Model.extend({
+	tile: null,
+	selector: null,
 
-    gameCard: function () {
-        "use strict";
-        return this._gameCard;
-    },
-    moreButton: function () {
-        "use strict";
-        return this._moreButton;
-    },
-    collectionPick: function () {
-        "use strict";
-        return this._collectionPick;
-    },
-
-    tileSteamClient: function (callback) {        "use strict";
-		this.ajaxTile(
-			'SteamClient.html', "#steam-client",
-			this._tileSteamClient, callback);
-    },
-
-    tileCollectionNew: function (callback) {        "use strict";
-		this.ajaxTile(
-			'CollectionNew.html', "#collection-new",
-			this._tileCollectionNew, callback);
-    },
-
-	tileCollectionEdit: function (callback) {		"use strict";
-		this.ajaxTile(
-			'CollectionEdit.html', "#collection-edit",
-			this._tileCollectionEdit, callback);
+	initialize: function() {
+		this.selector = this.attributes.selector;
 	},
 
-	tileCommandBox: function (callback) {		"use strict";
-		this.ajaxTile(
-			'CommandBox.html', "#command-box",
-			this._tileCommandBox, callback);
-	},
-
-	ajaxTile: function (url, selector, holder, callback) {
-		var had = holder.tile;
+	ajaxTile: function (url, callback) {
+		var had = this.tile;
 		if (had != null) {
 			callback(had);
 			return;
 		}
 		var that = this;
 		$.ajax({
-            url: url,
-            success: function (content) {
-			    that.storeTile(content, selector, holder, callback);
-		    },
-            error: function() {
-                console.log(arguments);
-            },
-            dataType: 'xml'});
+			url: url,
+			success: function (content) {
+				var xml = $(content);
+				var put = that.chomp(xml);
+				callback(put);
+			},
+			error: function() {
+				console.log(arguments);
+			},
+			dataType: 'xml'});
 	},
 
-	storeTile: function(content, selector, holder, callback) { "use strict";
-		var xml = $(content);
-		var put = this.xml2html(xml, selector);
-		holder.tile = put;
-		callback(put);
+	chomp: function(xml) {
+		var element = xml.find(this.selector);
+		this.tile = this.xml2html(element);
+		return this.tile;
+	},
+
+	xml2html: function(element) {
+		var outer = $('<div>').append(element.clone()).remove();
+		var full = outer.html();
+		return $(full);
+	}
+});
+
+var Tileset = {
+	_gameCard: new Tile({selector: ".game-tile"}),
+	_moreButton: new Tile({selector: ".more-button"}),
+	_collectionPick: new Tile({selector: ".collection-pick"}),
+	_tileSteamClient: new Tile({selector: "#steam-client"}),
+	_tileCollectionNew: new Tile({selector: "#collection-new"}),
+	_tileCollectionEdit: new Tile({selector: "#collection-edit"}),
+	_tileCommandBox: new Tile({selector: "#command-box"}),
+
+    gameCard: function () {
+        "use strict";
+        return this._gameCard.tile;
+    },
+    moreButton: function () {
+        "use strict";
+        return this._moreButton.tile;
+    },
+    collectionPick: function () {
+        "use strict";
+        return this._collectionPick.tile;
+    },
+
+    tileSteamClient: function (callback) {        "use strict";
+		this._tileSteamClient.ajaxTile(
+			'SteamClient.html',
+			 callback);
+    },
+
+    tileCollectionNew: function (callback) {        "use strict";
+		this._tileCollectionNew.ajaxTile(
+			'CollectionNew.html',
+			callback);
+    },
+
+	tileCollectionEdit: function (callback) {		"use strict";
+		this._tileCollectionEdit.ajaxTile(
+			'CollectionEdit.html',
+			callback);
+	},
+
+	tileCommandBox: function (callback) {		"use strict";
+		this._tileCommandBox.ajaxTile(
+			'CommandBox.html',
+			callback);
 	},
 
 	load: function (callback) {
         "use strict";
-        var path = 'tileset.html';
+        var url = 'tileset.html';
         var that = this;
-        $.ajax({url:path, success: function(template) {
-            var t = $(template);
-            that._gameCard   = that.xml2html(t, ".game-tile");
-            that._moreButton = that.xml2html(t, ".more-button");
-            that._collectionPick = that.xml2html(t, ".collection-pick");
-			callback(that);
-        }, dataType: 'xml'});
-    },
-    xml2html: function(xml, selector) {
-        var element = xml.find(selector);
-       	var outer = $('<div>').append(element.clone()).remove();
-        var full = outer.html();
-        return $(full);
+        $.ajax({
+			url: url,
+			success: function(template) {
+				var t = $(template);
+				that._gameCard.chomp(t);
+				that._moreButton.chomp(t);
+				that._collectionPick.chomp(t);
+				callback(that);
+        	},
+			error: function() {
+				console.log(arguments);
+			},
+			dataType: 'xml'});
     }
 };
