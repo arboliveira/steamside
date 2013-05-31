35d047e2e50cf1cd356da6c411760a8e9d8ff42d
André de Oliveira <andre@arbo.com.br>
2013-05-30 22:24:54 -0300

    Create empty collection with command hints

--- webui/src/main/resources/br/com/arbo/steamside/webui/wicket/CollectionEdit.js
+++ webui/src/main/resources/br/com/arbo/steamside/webui/wicket/CollectionEdit.js
@@ -19,11 +19,17 @@ var CollectionEditView = Backbone.View.extend({
 
     render: function() {		"use strict";
 
+		var that = this;
+
 		var collectionEditSearchResults = new SearchResults();
 
 		new SearchView({
 			el: this.$('#collection-edit-search-command-box'),
-			collection: collectionEditSearchResults
+			collection: collectionEditSearchResults,
+			on_CommandBox_rendered: function(viewCommandBox) { that.on_search_CommandBox_rendered(viewCommandBox) },
+			on_change_input: function(input) { that.on_search_input_changed(input) },
+			on_command: function(input) { that.on_search_command(input) },
+			on_command_alternate: function(input) { that.on_search_command_alternate(input) }
 		}).render();
 
 		var inCollection = new SteamsideCollectionApps();
@@ -36,5 +42,63 @@ var CollectionEditView = Backbone.View.extend({
 		fetch_json(inCollection);
 
         return this;
-    }
+    },
+
+	on_search_CommandBox_rendered: function(viewCommandBox) {
+		/*
+		viewCommandBox.emptyCommandHints();
+		viewCommandBox.appendCommandHint(this.tileSearchHintContinueA);
+		viewCommandBox.appendCommandHint(this.tileSearchHintSearchA);
+		viewCommandBox.appendCommandHintAlternate(this.tileSearchHintContinueB);
+		viewCommandBox.appendCommandHintAlternate(this.tileSearchHintSearchB);
+		viewCommandBox.focusInput();
+		*/
+	},
+
+	on_search_input_changed: function(input) {
+		/*
+		if (input == '') {
+			this.tileSearchHintContinueA.show();
+			this.tileSearchHintContinueB.show();
+			this.tileSearchHintSearchA.hide();
+			this.tileSearchHintSearchB.hide();
+		} else {
+			this.tileSearchHintContinueA.hide();
+			this.tileSearchHintContinueB.hide();
+			this.tileSearchHintSearchA.show();
+			this.tileSearchHintSearchB.show();
+			var selector = '#search-command-hint-subject';
+			this.tileSearchHintSearchA.find(selector).text(input);
+			this.tileSearchHintSearchB.find(selector).text(input);
+		}
+		*/
+	},
+
+	on_search_command: function(input) {
+		/*
+		if (input == '') {
+			var gameA = this.continues.at(0);
+			gameA.play();
+		} else {
+			this.searchResults.query = input;
+			fetch_json(this.searchResults);
+		}
+		*/
+	},
+
+	on_search_command_alternate: function(input) {
+		/*
+		if (input == '') {
+			var gameB = this.continues.at(1);
+			gameB.play();
+		} else {
+			var searchResults = this.searchResults;
+			searchResults.query = input;
+			fetch_json(searchResults, function() {
+				var first = searchResults.at(0);
+				first.play();
+			});
+		}
+		*/
+	}
 });
--- webui/src/main/resources/br/com/arbo/steamside/webui/wicket/CollectionNew.js
+++ webui/src/main/resources/br/com/arbo/steamside/webui/wicket/CollectionNew.js
@@ -5,68 +5,97 @@ var CollectionNewTile = {
 	ajaxTile: function (callback) {
 		this.tile.ajaxTile(callback);
 	}
-}
+};
 
 var CollectionNewEmptyView = Backbone.View.extend({
 
+	tileEmptyCommandHintA: null,
+	tileEmptyCommandHintB: null,
+
 	render: function() {
+		var tileEmptyCommandHint = this.$('#empty-command-hint');
+		tileEmptyCommandHint.remove();
+		this.tileEmptyCommandHintA = tileEmptyCommandHint.clone();
+		this.tileEmptyCommandHintB = tileEmptyCommandHint.clone();
+		var selectorAfterwards = '#empty-command-hint-afterwards';
+		this.tileEmptyCommandHintA.find(selectorAfterwards).text("add games");
+		this.tileEmptyCommandHintB.find(selectorAfterwards).text("stay here");
+
 		var targetEl = this.$('#div-empty-name-form');
 		targetEl.empty();
 
 		var that = this;
-
-		var on_command =
 
 		CommandBoxTile.ajaxTile(
 			function(tile) {
 				var view = new CommandBoxView({
 					el: tile.clone(),
 					placeholder_text: 'Name for empty collection',
-					on_command: function(input) { that.doCommand(input); },
-					on_change_input: function(input) { that.doChangeInput(input); }
+					on_command: function(input) { that.on_empty_command(input) },
+					on_command_alternate: function(input) { that.on_empty_command_alternate(input) },
+					on_change_input: function(input) { that.on_empty_change_input(input); }
 				});
 				targetEl.append(view.render().el);
-				that.doChangeInput("");
+
+				that.on_empty_CommandBox_rendered(view);
+
+				that.on_empty_change_input("");
 			}
 		);
 
 		return this;
 	},
 
-	doCommand: function(input) {     "use strict";
-		var aUrl = "/collection/" + input + "/create";
-		var after = "#/collections/" + input + "/edit";
+	on_empty_CommandBox_rendered: function(viewCommandBox) {
+		viewCommandBox.emptyCommandHints();
+		viewCommandBox.appendCommandHint(this.tileEmptyCommandHintA);
+		viewCommandBox.appendCommandHintAlternate(this.tileEmptyCommandHintB);
+		viewCommandBox.focusInput();
+	},
+
+	nameForCollection: function(input) {
+		if (input == '') return "Favorites";
+		return input;
+	},
+
+	on_empty_command: function(input) {
+		this.createEmpty({name: input, stay: false});
+	},
+
+	on_empty_command_alternate: function(input) {
+		this.createEmpty({name: input, stay: false});
+	},
+
+	createEmpty: function(args) {     "use strict";
+		var name = this.nameForCollection(args.name);
+		var aUrl = "/collection/" + name + "/create";
+		var that = this;
 
 		$.ajax({
 			url: aUrl,
 			dataType: dataTypeOf(aUrl),
 			beforeSend: function(){
-				//that.showOverlay('Now playing');
+				// TODO display 'creating...'
 			},
 			complete: function(){
-				Backbone.history.navigate(after, {trigger: true});
+				if (args.stay) {
+					var input_el = that.$('#input-text-command-box');
+					input_el.val('');
+					input_el.focus();
+					that.on_empty_change_input('');
+				} else {
+					Backbone.history.navigate(
+						"#/collections/" + name + "/edit",
+						{trigger: true});
+				}
 			}
 		});
-
-
-		/*
-		var c = this.collection;
-		c.value = input;
-		fetch_json(c);
-		*/
 	},
 
-    doChangeInput: function (input) {  "use strict";
-		var c_hint = this.$("#command-hint");
-		var c_hint_alternate = this.$("#command-hint-alternate");
-		if (input === '') {
-			var same = '(waiting for input...)';
-			c_hint.text(same);
-			c_hint_alternate.text(same);
-		} else {
-			var full = "Create " + input + " collection";
-			c_hint.text(full + " and start adding games");
-			c_hint_alternate.text(full + " and stay here");
-		}
+	on_empty_change_input: function (input) {  "use strict";
+		var name = this.nameForCollection(input);
+		var selector = '#empty-command-hint-subject';
+		this.tileEmptyCommandHintA.find(selector).text(name);
+		this.tileEmptyCommandHintB.find(selector).text(name);
     }
 });
