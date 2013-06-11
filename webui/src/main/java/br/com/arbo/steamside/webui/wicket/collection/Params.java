6a73fb19a92c06459309819f4da88286aab95d48
André de Oliveira <andre@arbo.com.br>
2013-06-11 01:22:20 -0300

    AppCollection JSON API with Spring

--- webui/src/main/java/br/com/arbo/steamside/webui/wicket/collection/Params.java
+++
@@ -1,8 +0,0 @@
-package br.com.arbo.steamside.webui.wicket.collection;
-
-public class Params {
-
-	public static final String PARAM_command = "command";
-	public static final String PARAM_collectionname = "collectionname";
-
-}
