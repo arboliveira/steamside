5772550758f0a73b7d057a5ab78a6b9a822e75cc
André de Oliveira <andre@arbo.com.br>
2013-06-10 22:35:55 -0300

    Wicket JsonResource completely replaced with Spring

--- webui/src/main/java/br/com/arbo/steamside/webui/wicket/json/Factory.java
+++
@@ -1,9 +0,0 @@
-package br.com.arbo.steamside.webui.wicket.json;
-
-import org.apache.wicket.request.resource.IResource.Attributes;
-
-public interface Factory<T> {
-
-	T produce(Attributes a);
-
-}
