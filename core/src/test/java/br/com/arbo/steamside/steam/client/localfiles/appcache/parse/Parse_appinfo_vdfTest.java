091bf38bbeea980b9838b02090a413949c1f9a27
André de Oliveira <andre@arbo.com.br>
2013-12-04 21:34:20 -0200

    Known ids vs names in a separate file

--- core/src/test/java/br/com/arbo/steamside/steam/client/localfiles/appcache/parse/Parse_appinfo_vdfTest.java
+++ core/src/test/java/br/com/arbo/steamside/steam/client/localfiles/appcache/parse/Parse_appinfo_vdfTest.java
@@ -1,13 +1,19 @@
 package br.com.arbo.steamside.steam.client.localfiles.appcache.parse;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
+import java.util.List;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -48,11 +54,38 @@ public class Parse_appinfo_vdfTest {
 				}).parse();
 	}
 
-	private static HashMap<String, String> id_vs_name() {
+	private HashMap<String, String> id_vs_name() {
 		final HashMap<String, String> m = new HashMap<String, String>();
-		m.put("6870", "Battlestations: Midway");
-		m.put("12210", "Grand Theft Auto IV");
-		m.put("55230", "Saints Row: The Third");
+		load_id_vs_name(m);
+		assertThat(m.size(), is(not(0)));
 		return m;
+	}
+
+	private void load_id_vs_name(final HashMap<String, String> m) {
+		try {
+			load_id_vs_name_try(m);
+		} catch (final IOException e) {
+			throw new RuntimeException();
+		}
+	}
+
+	private void load_id_vs_name_try(final HashMap<String, String> m)
+			throws IOException {
+		final InputStream in = this.getClass().getResourceAsStream(
+				"known_id_vs_name.txt");
+		try {
+			load_id_vs_name_try(m, in);
+		} finally {
+			in.close();
+		}
+	}
+
+	private static void load_id_vs_name_try(final HashMap<String, String> m,
+			final InputStream in) throws IOException {
+		final List<String> lines = IOUtils.readLines(in);
+		for (final String line : lines) {
+			final String[] strings = StringUtils.split(line, '\t');
+			m.put(strings[0], strings[1]);
+		}
 	}
 }
