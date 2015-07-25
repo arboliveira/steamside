3fde3ade342f10956ee1eaa1ff268e467b25c43d
André de Oliveira <andre@arbo.com.br>
2015-07-25 19:12:52 -0300

    Avoid crashes due to recent Steam changes to appinfo.vdf

--- steamclient/src/main/java/br/com/arbo/steamside/steam/client/localfiles/appcache/KeyValues_cpp.java
+++ steamclient/src/main/java/br/com/arbo/steamside/steam/client/localfiles/appcache/KeyValues_cpp.java
@@ -4,25 +4,33 @@ import static br.com.arbo.steamside.steam.client.localfiles.appcache.KeyValues_h
 import static br.com.arbo.steamside.steam.client.localfiles.appcache.KeyValues_h.TYPE_NONE;
 import static br.com.arbo.steamside.steam.client.localfiles.appcache.KeyValues_h.TYPE_NUMTYPES;
 import static br.com.arbo.steamside.steam.client.localfiles.appcache.KeyValues_h.TYPE_STRING;
+import static br.com.arbo.steamside.steam.client.localfiles.appcache.KeyValues_h.TYPE_UINT64;
+
 import br.com.arbo.steamside.steam.client.vdf.KeyValueVisitor;
+import br.com.arbo.steamside.steam.client.vdf.KeyValueVisitor.Finished;
 import br.com.arbo.steamside.steam.client.vdf.NotFound;
 import br.com.arbo.steamside.steam.client.vdf.Region;
-import br.com.arbo.steamside.steam.client.vdf.KeyValueVisitor.Finished;
 
 /**
 https://github.com/ValveSoftware/source-sdk-2013/blob/master/mp/src/tier1/KeyValues.cpp
  */
-public class KeyValues_cpp {
+public class KeyValues_cpp
+{
 
 	public static void readAsBinary(
-			final ByteBufferX buffer,
-			final KeyValueVisitor visitor) {
-		while (true) {
+		final ByteBufferX buffer,
+		final KeyValueVisitor visitor)
+	{
+		while (true)
+		{
 			final byte type = buffer.getUnsignedChar();
 			if (type == TYPE_NUMTYPES) break;
-			try {
+			try
+			{
 				readNameValue(buffer, visitor, type);
-			} catch (final Finished e) {
+			}
+			catch (final Finished e)
+			{
 				break;
 			}
 		}
@@ -30,36 +38,45 @@ public class KeyValues_cpp {
 	}
 
 	private static void readNameValue(final ByteBufferX buffer,
-			final KeyValueVisitor visitor, final byte type) throws Finished {
-		class RegionImpl implements Region {
+		final KeyValueVisitor visitor, final byte type) throws Finished
+	{
+		class RegionImpl implements Region
+		{
 
 			@Override
-			public void accept(final KeyValueVisitor visitor) {
+			public void accept(final KeyValueVisitor visitor)
+			{
 				readAsBinary(buffer, visitor);
 			}
 
 			@Override
-			public Region region(String name) throws NotFound {
+			public Region region(String name) throws NotFound
+			{
 				throw new UnsupportedOperationException();
 			}
 
 		}
 
 		final String name = buffer.read__null_terminated_string();
-		switch (type) {
-			case TYPE_NONE:
-				visitor.onSubRegion(name, new RegionImpl());
-				break;
-			case TYPE_STRING:
-				final String string = buffer.read__null_terminated_string();
-				visitor.onKeyValue(name, string);
-				break;
-			case TYPE_INT:
-				final int int32 = buffer.read__uint32_t();
-				visitor.onKeyValue(name, String.valueOf(int32));
-				break;
-			default:
-				throw new UnexpectedType(type, name);
+		switch (type)
+		{
+		case TYPE_NONE:
+			visitor.onSubRegion(name, new RegionImpl());
+			break;
+		case TYPE_STRING:
+			final String string = buffer.read__null_terminated_string();
+			visitor.onKeyValue(name, string);
+			break;
+		case TYPE_INT:
+			final int uint32 = buffer.read__uint32_t();
+			visitor.onKeyValue(name, String.valueOf(uint32));
+			break;
+		case TYPE_UINT64:
+			final long uint64 = buffer.read__uint64_t();
+			visitor.onKeyValue(name, String.valueOf(uint64));
+			break;
+		default:
+			throw new UnexpectedType(type, name);
 		}
 	}
 
--- steamclient/src/main/java/br/com/arbo/steamside/steam/client/localfiles/appcache/KeyValues_h.java
+++ steamclient/src/main/java/br/com/arbo/steamside/steam/client/localfiles/appcache/KeyValues_h.java
@@ -3,11 +3,13 @@ package br.com.arbo.steamside.steam.client.localfiles.appcache;
 /**
 https://github.com/ValveSoftware/source-sdk-2013/blob/master/mp/src/public/tier1/KeyValues.h
  */
-public class KeyValues_h {
+public class KeyValues_h
+{
 
 	public static final int TYPE_NONE = 0;
 	public static final int TYPE_STRING = 1;
 	public static final int TYPE_INT = 2;
+	public static final int TYPE_UINT64 = 7;
 	public static final int TYPE_NUMTYPES = 8;
 
 }
